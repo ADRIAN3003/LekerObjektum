b68aa8915af2c495d2a56ab615fa84773d6f4cbe
ADRIAN30 <medveadrian.szgya@gmail.com>
2022-10-24 14:34:06 +0200

    create Vevo class

--- LekerObjektum/Program.cs
+++ LekerObjektum/Program.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace LekerObjektum
 {
+    class Vevo
+    {
+        public string nev;
+        public string megye;
+        public string email;
+
+        public Vevo(string nev, string megye, string email)
+        {
+            this.nev = nev;
+            this.megye = megye;
+            this.email = email;
+        }
+    }
+
     class Program
     {
+        List<Vevo> vevos = new List<Vevo>() {
+            new Vevo("Nagy János", "Nógrád", "nagyj@gmail.com")
+        };
         static void Main(string[] args)
         {
         }
