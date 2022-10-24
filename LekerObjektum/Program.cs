ef461570912cac79f307da25ae061ccafd412567
ADRIAN30 <medveadrian.szgya@gmail.com>
2022-10-24 14:45:33 +0200

    complete task

--- LekerObjektum/Program.cs
+++ LekerObjektum/Program.cs
@@ -22,11 +22,28 @@ namespace LekerObjektum
 
     class Program
     {
-        List<Vevo> vevos = new List<Vevo>() {
-            new Vevo("Nagy János", "Nógrád", "nagyj@gmail.com")
+        static List<Vevo> vevos = new List<Vevo>() {
+            new Vevo("Nagy János", "Nógrád", "nagyj@gmail.com"),
+            new Vevo("Kiss Ildikó", "Budapest", "kiss@gmail.com"),
+            new Vevo("Híves Elemér", "Nógrád", "hives@gmail.com"),
+            new Vevo("Tuza Enikő", "Zala", "tuza@gmail.com"),
+            new Vevo("Telek Tibor", "Nógrád", "telek@gmail.com"),
         };
         static void Main(string[] args)
         {
+            var vevok = from vevo in vevos
+                        group vevo by vevo.megye;
+
+            foreach (var vevo in vevok)
+            {
+                Console.WriteLine(vevo.Key + $" ({vevo.Count()} db):");
+                foreach (var item in vevo)
+                {
+                    Console.WriteLine("\t" + item.nev);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
